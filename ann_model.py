4f5cb2da50c9b4ab9227286977b31075040a33e9
Kayam Kushmita <kushmitaforwork@gmail.com>
2025-04-28 11:49:59 +0530

    Create ann_model.py

---
+++ ann_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class ANN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(ANN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
